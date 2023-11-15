1cf8e318f0a5a1ec727ce783469477db1d283d03
Anju-Kurian-08 <150518861+Anju-Kurian-08@users.noreply.github.com>
2023-11-15 20:02:10 +0530

    sampleheloo

---
+++ samplemaven/src/main/java/sampleheloomaven/Hello_Sample.java
@@ -0,0 +1,10 @@
+package sampleheloomaven;
+
+public class Hello_Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("this is my sample project to clone to github");
+	}
+
+}
