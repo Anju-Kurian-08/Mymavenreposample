fe323b97c762029f917069050910e98472251693
Anju-Kurian-08 <150518861+Anju-Kurian-08@users.noreply.github.com>
2023-12-04 23:09:29 +0530

    go

---
+++ mymavenproject/src/main/java/abstraction/Abstraction_1.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public abstract class Abstraction_1 {
+	
+	public abstract void sample(); // abstract method
+	
+    public void sample2()
+    {
+    	System.out.println("this is a normal method in abstract class");
+    }
+}
